67381376efca1a38424439c00356f9732c68fbb2
NoureenAyman <noureenayman7@gmail.com>
2024-12-01 23:27:11 +0200

    Initial commit

---
+++ lib/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color primaryColor = Color(0xff5D9CEC);
+
+}
---
+++ lib/home_screen.dart
@@ -0,0 +1,20 @@
+import'package:flutter/material.dart';
+
+import 'app_colors.dart';
+
+
+
+class HomeScreen extends StatelessWidget{
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.primaryColor,
+    );
+  }
+
+
+
+
+}
